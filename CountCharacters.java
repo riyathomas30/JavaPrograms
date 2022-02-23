5550b39272128424ae7b4a0a4dc3d1e89a073679
riyathomas30 <98868941+riyathomas30@users.noreply.github.com>
2022-02-23 16:10:20 +0400

    Add files via upload

---
+++ CountCharacters.java
@@ -0,0 +1,38 @@
+
+public class CountCharacters {
+	static  int ASCII_SIZE = 256;
+	public static void main(String[] args) {
+		String str="mathematics";
+//		
+//	        
+//	        
+		 int count[] = new int[ASCII_SIZE];
+		  for(int i=0; i<str.length(); i++)
+		  {
+			  (count[str.charAt(i)])++; //increment the count of each character by using ASCII of character as key  
+		  }
+		  
+		  int first = 0, second =0;
+		  for(int i=0; i < ASCII_SIZE; i++)
+		  {
+		   
+		    if(count[i] > count[first]) //
+		    {
+		      second = first;
+		      first = i;
+		    }
+		   
+		    else if(count[i] > count[second] && count[i] != count[first])
+		    {
+		      second = i;
+		    }
+		    
+		  }
+
+		 
+		   System.out.println("second most frequent character is "+ (char)second);  	  
+		   System.out.println(" most frequent character is "+ (char)first); 
+		 
+	}
+
+}
